43682e54c74acea84cc374f7a91939b0ea9194e6
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-11-09 14:32:38 +0600

    added the limited service frist three

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
             res.send(services);
         })
 
+        app.get('/home', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+
         app.get('/services/:id', async (req, res) => {
 
             const id = req.params.id;
