167c00071646f53db3c26dd903436122a7ba2503
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-11-10 21:00:10 +0600

    added the review delete api of the mongodb

--- index.js
+++ index.js
@@ -90,7 +90,12 @@ async function run() {
         })
 
 
-
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
